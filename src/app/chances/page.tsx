b2c85f1816d2b420d803f244c94148837cf536e1
opal2718 <littlesihoo@gmail.com>
2025-05-30 10:58:00 +0900

    debugging

--- src/app/chances/page.tsx
+++ src/app/chances/page.tsx
@@ -2,7 +2,6 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import Header from '@/components/Header';
 import { useState } from "react";
 
